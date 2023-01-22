68825808960cfe6b8b9c97aa5090c4e9aa36ad35
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-01-22 02:31:11 +0100

    Docker

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     config: {
         host: 'mysql_db', // the host name MYSQL_DATABASE: node_mysql
-        user: 'benake', // database user MYSQL_USER: MYSQL_USER
-        password: 'palkast1', // database user password MYSQL_PASSWORD
+        user: 'MYSQL_USER', // database user MYSQL_USER: MYSQL_USER
+        password: 'MYSQL_PASSWORD', // database user password MYSQL_PASSW
         database: 'microgreens',
         port: 3306
     }
