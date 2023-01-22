4a310533b671116e4d1367771c69dc0c5a975b39
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-01-22 14:30:54 +0100

    Docker

--- server.js
+++ server.js
@@ -75,6 +75,7 @@ app.get('/shelves', db.getShelves);
 const port = process.env.PORT || 3001,
     ip = process.env.IP || '127.0.0.1';
 
+    app.listen('3001', () => { })
 //http.listen(port);
 //console.log('Server running on http://%s:%s', ip,port);
 console.log('NODEJS RUNNING')
