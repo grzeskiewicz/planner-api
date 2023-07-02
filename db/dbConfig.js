daa616bc3bbe98d8705919b728bb8323fc4e1733
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-07-02 23:34:48 +0200

    Server

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     config: {
-        host: 'localhost', // the host name MYSQL_DATABASE: node_mysql
-        user: 'root', // database user MYSQL_USER: MYSQL_USER
-        password: '', // database user password MYSQL_PASSW
+        host: '192.168.1.35', // the host name MYSQL_DATABASE: node_mysql
+        user: 'ben', // database user MYSQL_USER: MYSQL_USER
+        password: 'Palkast123!', // database user password MYSQL_PASSW
         database: 'microgreens',
         port: 3306
     }
