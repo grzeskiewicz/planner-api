58b420dba98754519a5d834a1d65e40eeb898b68
unknown <ariel.grzeskiewicz@gmail.com>
2023-09-14 23:05:56 +0200

    Changed IP db

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     config: {
-        host: '192.168.0.200', // the host name MYSQL_DATABASE: node_mysql
+        host: '192.168.0.145', // the host name MYSQL_DATABASE: node_mysql
         user: 'ben', // database user MYSQL_USER: MYSQL_USER
         password: 'Palkast123!', // database user password MYSQL_PASSW
         database: 'microgreens',
